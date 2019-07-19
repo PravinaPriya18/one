62d81b1efb7d6d00327c2436d44e0c7bdf9b50cc
PravinaPriya18 <51355367+PravinaPriya18@users.noreply.github.com>
2019-07-19 12:55:23 +0530

    Update pravinapriya.py

--- pravinapriya.py
+++ pravinapriya.py
@@ -1,6 +1,8 @@
-p=['a','e','i','o','u']
-q=int(input())
-if q in p():
- print('vowel')
- else:
-  print('consonant')
+p=input()
+if((p<='A' and p<='Z') or (p<='a' and p<='z')):
+ if(p=='a',p=='e',p=='i',p=='o',p=='u',p=='A',p=='E',p=='I',p=='O',p=='U'):
+  print('vowel')
+  else:
+   print('Consonant')
+else:
+  print('invalid')
